c8891b7c6700df922d3593b56a548b99adbf04f6
kr-master4610 <master4610@gmail.com>
2026-05-05 13:26:50 +0200

    homework_7

--- djangoproject1/settings.py
+++ djangoproject1/settings.py
@@ -40,11 +40,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-(7&^oa7p@1f)0zq2&!tz!y*e01bnhf8ohz9d19-xwb1aem96pa'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = env.str('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
--- djangoproject1/urls.py
+++ djangoproject1/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from services.views import greetings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home-page/', greetings)
 ]
